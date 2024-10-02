3560c4c6e36bbaca24c70ac395b820b844b4792a
vickyerie <vickyerie.s@gmail.com>
2024-10-02 16:11:52 +0700

    Update drawer.dart

--- lib/app/modules/home/views/drawer.dart
+++ lib/app/modules/home/views/drawer.dart
@@ -2,63 +2,103 @@ import 'package:comic_app/app/helpers/const.dart';
 import 'package:comic_app/app/helpers/themes.dart';
 import 'package:comic_app/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 class HomeDrawer extends StatelessWidget {
-  const HomeDrawer({
-    super.key,
-  });
+  const HomeDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Column(
-        children: <Widget>[
-          // User Account Information
-          UserAccountsDrawerHeader(
-            accountName: Text(dummyUsername),
-            accountEmail: Text(dummyEmail),
-            currentAccountPicture: CircleAvatar(
-              backgroundImage: AssetImage(imgProfile),
-            ),
-            decoration: BoxDecoration(
-              color: getTheme(context).primaryColor,
-            ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.horizontal(right: Radius.circular(24)),
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              getTheme(context).colorScheme.primary.withOpacity(0.1),
+              getTheme(context).colorScheme.secondary.withOpacity(0.1),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
           ),
-          // List of items or settings
-          Expanded(
-            child: ListView(
-              padding: EdgeInsets.zero,
-              children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.person),
-                  title: Text('Profile'),
-                  onTap: () {
-                    // Handle profile tap
-                  },
+        ),
+        child: Column(
+          children: <Widget>[
+            // User Account Information
+            UserAccountsDrawerHeader(
+              accountName: Text(
+                dummyUsername,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text('Settings'),
-                  onTap: () {
-                    // Handle settings tap
-                  },
+              ),
+              accountEmail: Text(
+                dummyEmail,
+                style: TextStyle(color: Colors.white),
+              ),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage(imgProfile),
+              ),
+              decoration: BoxDecoration(
+                color: getTheme(context).primaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
                 ),
-              ],
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListTile(
-              leading: Icon(Icons.logout),
-              title: Text('Sign Out'),
-              onTap: () {
-                Get.offAllNamed(Routes.LOGIN);
-              },
+            // List of items or settings
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  ListTile(
+                    leading: const Icon(Icons.person),
+                    title: const Text('Profile'),
+                    onTap: () {
+                      // Handle profile tap
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.settings),
+                    title: const Text('Settings'),
+                    onTap: () {
+                      // Handle settings tap
+                    },
+                  ),
+                  const Divider(), // Adding a divider for better separation
+                  ListTile(
+                    leading: const Icon(Icons.help),
+                    title: const Text('Help'),
+                    onTap: () {
+                      // Handle help tap
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.info),
+                    title: const Text('About'),
+                    onTap: () {
+                      // Handle about tap
+                    },
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ListTile(
+                leading: const Icon(Icons.logout),
+                title: const Text('Sign Out'),
+                onTap: () {
+                  Get.offAllNamed(Routes.LOGIN);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
